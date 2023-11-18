2117a455dd989c821917d4c310a7fee06a7b5a3a
Abhishek Bhosale <58450561+AbhishekBhosale46@users.noreply.github.com>
2023-11-19 01:57:35 +0530

    Modified Socket Programming

--- LP1/DS/Socket Programming/client.py
+++ LP1/DS/Socket Programming/client.py
@@ -1,22 +1,18 @@
 import socket
 
-# DEFINE IP, PORT, ETC
-HEADER = 64
-PORT = 5050
-SERVER = '172.25.240.1'
-ADDRESS = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MSG = '!DISCONNECT'
+def send_data():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 8888))
+    print()
+    message = input("Enter message to echo : ")
+    client_socket.send(message.encode('utf-8'))
+    response = client_socket.recv(1024)
+    print(f"[!] Received echo from server : {response.decode('utf-8')}")
+    print()
+    client_socket.close()
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDRESS)
+def main():
+    send_data()
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_leng = len(message)
-    send_leng = str(msg_leng).encode(FORMAT)
-    send_leng += b' '*(HEADER-len(send_leng))
-    client.send(send_leng)
-    client.send(message)
-
-send("Hello World!")
+if __name__ == "__main__":
+    main()
--- LP1/DS/Socket Programming/server.py
+++ LP1/DS/Socket Programming/server.py
@@ -1,49 +1,28 @@
 import socket
 import threading
 
+def handle_client(client_socket, address):
+    echo_req = client_socket.recv(1024)
+    print(f"[-] Received from {address} : {echo_req.decode('utf-8')}")
+    client_socket.send(echo_req)
+    client_socket.close()
 
-# DEFINE IP, PORT, ETC
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDRESS = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MSG = '!DISCONNECT'
-
-
-# CREATE SERVER AND BIND IT TO A IP & PORT
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDRESS)
-
-
-# FUNCTION TO HANDLE INDIVIDUAL CLIENT LOGIN AFTER START(), WILL RUN CONCURRENTLY FOR EACH CLIENT
-def handle_client(conn,addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-    connected = True
-    while connected:
-        msg_leng = conn.recv(HEADER).decode(FORMAT)
-        if msg_leng:
-            msg_leng = int(msg_leng)
-            msg = conn.recv(msg_leng).decode(FORMAT)
-            if msg == DISCONNECT_MSG:
-                connected = False
-            print(f"[{addr}] {msg}")
-    conn.close()
-
-
-def start():
-    # START LISTENING ON BINDED IP & PORT
-    server.listen()
-    print(f"[LISTENING] server is listening on {SERVER}")
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 8888))
+    server_socket.listen()
+    print()
+    print("[#] Server listening on port 8888.... ")
     while True:
-        # CONN IS SOCKET OBJ USED TO COMMUNICATE BACK TO CLIENT, ADDR IS INFO OF CONNECTION
-        conn, addr = server.accept()
-        # CREATING THREADS FOR EVERY CONNECTED CLIENT, SO THAT OTHER CLIENTS NOT NEED TO WAIT
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        # PRINTS ACTIVE CONNECTIONS
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}.")
+        client_socket, address = server_socket.accept()
+        print()
+        print(f"[!] Connected with {address}")
+        client_handler = threading.Thread(target=handle_client, args=(client_socket, address))
+        client_handler.start()
 
+def main():
+    start_server()
 
-print(f"[STARTING] server starting ...")
-start()
+if __name__=="__main__":
+    main()
+
